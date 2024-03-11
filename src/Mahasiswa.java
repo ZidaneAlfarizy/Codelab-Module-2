3c6063412c46313117181f18de8e2c203e0e8f1a
ZidaneAlfarizy <alfarizy143@gmail.com>
2024-03-12 02:00:13 +0700

    first commit

---
+++ src/Mahasiswa.java
@@ -0,0 +1,21 @@
+public class Mahasiswa {
+    private String nama;
+    private String nim;
+    private String jurusan;
+
+    public Mahasiswa(String nama, String nim, String jurusan) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jurusan = jurusan;
+    }
+
+    public static void tampilUniversitas() {
+        System.out.println("Universitas Muhammadiyah Malang");
+    }
+
+    public void tampilDataMahasiswa() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Mahasiswa> daftarMahasiswa = new ArrayList<>();
+
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Tambah Data Mahasiswa");
+            System.out.println("2. Tampilkan Data Mahasiswa");
+            System.out.println("3. Keluar");
+            System.out.print("Pilihan Anda: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    tambahDataMahasiswa(scanner, daftarMahasiswa);
+                    break;
+                case 2:
+                    Mahasiswa.tampil1Universitas();
+                    System.out.println("Data Mahasiswa:");
+                    for (Mahasiswa mahasiswa : daftarMahasiswa) {
+                        mahasiswa.tampilDataMahasiswa();
+                    }
+                    break;
+                case 3:
+                    System.out.println("Adios");
+                    System.exit(0);
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan pilih 1, 2, atau 3.");
+                    break;
+            }
+        }
+    }
+
+    public static void tambahDataMahasiswa(Scanner scanner, ArrayList<Mahasiswa> daftarMahasiswa) {
+        scanner.nextLine(); // Consume newline character
+        System.out.print("Masukkan nama mahasiswa: ");
+        String nama = scanner.nextLine();
+        System.out.print("Masukkan NIM mahasiswa: ");
+        String nim = scanner.nextLine();
+        while (nim.length() != 15) {
+            System.out.println("NIM Harus 15 Digit!!!");
+            System.out.print("Masukkan NIM mahasiswa: ");
+            nim = scanner.nextLine();
+        }
+        System.out.print("Masukkan jurusan mahasiswa: ");
+        String jurusan = scanner.nextLine();
+        Mahasiswa mahasiswa = new Mahasiswa(nama, nim, jurusan);
+        daftarMahasiswa.add(mahasiswa);
+        System.out.println("Data mahasiswa berhasil ditambahkan.");
+    }
+}
